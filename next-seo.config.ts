91c4095e41e40f3887c4ea1b8147ac485f22eeda
masterjanic <68118279+masterjanic@users.noreply.github.com>
2023-06-23 09:41:39 +0200

    chore: update seo

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -10,11 +10,22 @@ const config: DefaultSeoProps = {
   openGraph: {
     type: "website",
     locale: "en_US",
+    url: "https://discord-checker.janic.dev/",
     siteName: title,
+    title: "Check your Discord Tokens easily",
     description,
+    images: [
+      {
+        url: "https://discord-checker.janic.dev/og.png",
+        width: 1200,
+        height: 630,
+        alt: title,
+        type: "image/png",
+      },
+    ],
   },
   twitter: {
-    cardType: "summary",
+    cardType: "summary_large_image",
   },
   themeColor: "#5865F2",
   additionalMetaTags: [
